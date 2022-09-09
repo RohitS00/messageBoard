cc5edbb2f18b20c8ef3e268cf5c53c6050501db0
Rohit <rohitbe5e@gmail.com>
2022-09-09 11:56:07 +0530

    heroku stuff

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn messageBoard.wsgi --log-file -
--- messageBoard/settings.py
+++ messageBoard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4qu4i4g_i#fncq#mk@l2$gk&39--v8oeiy)7l=#3(iuyu*p6=&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
